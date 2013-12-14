56c1870a5757a04381240cf3060ea48df0b724d0
Hans Nielsen <hans@stackallocated.com>
2013-12-13 20:31:41 -0800

    Client can now get tokens from imgur servers

---
+++ imgurexport.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require "rubygems"
+require "bundler/setup"
+
+require "imgur"
+
+class ImgurExport
+  def initialize(username=nil, **opts)
+    opts[:config_path] ||= File.expand_path("imgur.rc", File.dirname(__FILE__))
+    @client = Imgur::Client.new(opts)
+    @client.config[:account_name] ||= username
+  end
+
+  def get_all_images
+    @client.images.all
+  end
+end
+
+if __FILE__ == $0
+  ie = ImgurExport.new()
+  puts ie.get_all_images
+end
